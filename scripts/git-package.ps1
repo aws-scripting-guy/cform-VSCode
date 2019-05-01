3c40e145361e35ae74fca497225e6773768c2d16
AWS Scripting Guy <aws-scripting-guy@outlook.com>
2019-05-01 17:04:31 +0200

    adding new resources to support Azure DevOps CI

---
+++ scripts/git-package.ps1
@@ -0,0 +1,14 @@
+# Write package.json back to github only for release builds 
+
+write-host "[info] Build reason is: $env:BUILD_REASON "
+
+if ($env:BUILD_REASON -eq "Schedule") {
+  # write package.json back to repo 
+  git show
+  git log --oneline
+  git push 
+}
+else {
+  # do nothing    
+  write-host "[info] this is not a scheduled build. Build reason is $env:BUILD_REASON "
+}
